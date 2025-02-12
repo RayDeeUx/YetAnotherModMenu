8e7fe1515dacee4dc2f2298f0c8adf63518d12c9
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-12 14:04:48 -0500

    add pauselayer checks
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/FMODAudioEngine.cpp
+++ src/FMODAudioEngine.cpp
@@ -11,7 +11,10 @@ class $modify (MyFMODAudioEngine, FMODAudioEngine) {
 	void playEffect(gd::string p0, float p1, float p2, float p3) {
 		Manager* manager = Manager::getSharedInstance();
 		std::string desiredPath = manager->filthyPath.string();
-		if (!manager->filthyGameplay && GJBaseGameLayer::get()) desiredPath = p0;
+		if (!manager->filthyGameplay && GJBaseGameLayer::get()) {
+			if (!CCScene::get()->getChildByType<PauseLayer*>(0)) desiredPath = p0;
+			if (!CCScene::get()->getChildByType<EditorPauseLayer*>(0)) desiredPath = p0;
+		}
 		if (!std::filesystem::exists(manager->filthyPath)) desiredPath = p0;
 		if (manager->filthyPath.string().empty()) desiredPath = p0;
 		if (!Utils::isSupportedFMODExtension(manager->filthyPath.string())) desiredPath = p0;
