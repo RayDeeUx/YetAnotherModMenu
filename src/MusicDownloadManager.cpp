b04108d92f5dae103b0635626382cbfd0160c80e
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-12 14:03:22 -0500

    call original first imo
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/MusicDownloadManager.cpp
+++ src/MusicDownloadManager.cpp
@@ -10,10 +10,12 @@ class $modify (MyMusicDownloadManager, MusicDownloadManager) {
 	}
 	gd::string pathForSFX(int p0) {
 		Manager* manager = Manager::getSharedInstance();
-		if (!manager->filthyGameplay && GJBaseGameLayer::get()) return MusicDownloadManager::pathForSFX(p0);
-		if (!std::filesystem::exists(manager->filthyPath)) return MusicDownloadManager::pathForSFX(p0);
-		if (manager->filthyPath.string().empty()) return MusicDownloadManager::pathForSFX(p0);
-		if (!Utils::modEnabled() || !manager->filth) return MusicDownloadManager::pathForSFX(p0);
+		const std::string& originalPath = MusicDownloadManager::pathForSFX(p0);
+		if (!manager->filthyGameplay && GJBaseGameLayer::get()) return originalPath;
+		if (!std::filesystem::exists(manager->filthyPath)) return originalPath;
+		if (manager->filthyPath.string().empty()) return originalPath;
+		if (!Utils::isSupportedFMODExtension(manager->filthyPath.string())) return originalPath;
+		if (!Utils::modEnabled() || !manager->filth) return originalPath;
 		return manager->filthyPath.string();
 		// return "sfx/s4451.ogg";
 	}
