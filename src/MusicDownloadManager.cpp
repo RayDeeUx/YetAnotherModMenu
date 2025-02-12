200aef22114045e2cacd7b94b0a34a34bf065d36
Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-02-12 13:17:43 -0500

    redo approach for SFX overriding
    Signed-off-by: Erymanthus[#5074] | (u/)RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>

--- src/MusicDownloadManager.cpp
+++ src/MusicDownloadManager.cpp
@@ -10,9 +10,6 @@ class $modify (MyMusicDownloadManager, MusicDownloadManager) {
 	}
 	gd::string pathForSFX(int p0) {
 		Manager* manager = Manager::getSharedInstance();
-		log::info("p0: {}", p0);
-		log::info("manager->filthyPath: {}", manager->filthyPath);
-		log::info("std::filesystem::exists(manager->filthyPath): {}", std::filesystem::exists(manager->filthyPath));
 		if (!manager->filthyGameplay && GJBaseGameLayer::get()) return MusicDownloadManager::pathForSFX(p0);
 		if (!std::filesystem::exists(manager->filthyPath)) return MusicDownloadManager::pathForSFX(p0);
 		if (manager->filthyPath.string().empty()) return MusicDownloadManager::pathForSFX(p0);
